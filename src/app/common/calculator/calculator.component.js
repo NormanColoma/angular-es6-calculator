3b37e6c1cf6f659fc133c3eaf0f3c0391a08f982
Norman <ua.norman@gmail.com>
2017-01-28 13:47:30 +0100

    Calculator component added

--- src/app/common/calculator/calculator.component.js
+++ src/app/common/calculator/calculator.component.js
@@ -1,5 +1,55 @@
 const componentTemplate = require('./calculator.component.pug');
 
+export class CalculatorComponentCtrl {
+  constructor(){
+  }
+
+  $onInit() {
+    this.BASE = 'base';
+    this.IVA = 'iva';
+    this.TOTAL = 'Total';
+  }
+
+  valueChanged(value) {
+    switch(value) {
+        case this.BASE:
+          this.baseChanged();
+          break;
+        case this.IVA:
+          this.ivaChanged();
+          break;
+        case this.TOTAL:
+          this.totalChanged();
+          break;
+    }
+  }
+
+  ivaChanged() {
+    if (angular.isDefined(this.base) && this.base != '') {
+      this.total = Math.round(this.base * (1+this.iva/100));
+    } else if (angular.isDefined(this.total) && this.total !='') {
+      this.base = Math.round(this.total / (1+this.iva/100));
+    }
+  }
+
+  baseChanged() {
+    if (angular.isDefined(this.iva) && this.base != '') {
+        this.total = Math.round(this.base * (1+this.iva/100));
+    } else if(angular.isDefined(this.total)) {
+      this.total = '';
+    }
+  }
+
+  totalChanged() {
+    if (angular.isDefined(this.iva) && this.total != '') {
+        this.base = Math.round(this.total / (1+this.iva/100));
+    } else if(angular.isDefined(this.base)) {
+        this.base = '';
+    }
+  }
+}
+
 export const CalculatorComponent = {
+  controller: CalculatorComponentCtrl,
   template: componentTemplate,
 };
