5a16bff0dedb201db58d40c60649190f25a7137e
Norman Coloma <ua.norman@gmail.com>
2017-02-24 15:39:45 +0100

    Register link added to login view

--- src/app/common/login/login.module.js
+++ src/app/common/login/login.module.js
@@ -2,6 +2,8 @@ import angular from 'angular';
 
 import { LoginComponent } from './login.component';
 
+import './login.component.scss';
+
 export const LoginModule = angular
     .module('login.module', [])
     .component('loginComponent', LoginComponent)
